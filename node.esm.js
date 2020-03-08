73a7fe701811911294f5effeafce8dc623d05857
nin-jin <nin-jin@users.noreply.github.com>
2020-03-08 07:04:24 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -4966,7 +4966,7 @@ var $;
         version() {
             return this.stat().mtime.getTime().toString(36).toUpperCase();
         }
-        exists(next) {
+        exists(next, force) {
             let exists = true;
             try {
                 this.stat();
@@ -5077,6 +5077,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "version", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_file.prototype, "exists", null);
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "type", null);
--- node.test.js
+++ node.test.js
@@ -4959,7 +4959,7 @@ var $;
         version() {
             return this.stat().mtime.getTime().toString(36).toUpperCase();
         }
-        exists(next) {
+        exists(next, force) {
             let exists = true;
             try {
                 this.stat();
@@ -5070,6 +5070,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "version", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_file.prototype, "exists", null);
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "type", null);
