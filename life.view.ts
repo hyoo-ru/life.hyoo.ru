4025545504299d9a3cc26ebdeb944dcb6f15c26f
jin <jin@hyoo.ru>
2022-09-25 17:06:27 +0300

    Store snapshot when start life

--- life.view.ts
+++ life.view.ts
@@ -10,6 +10,12 @@ namespace $.$$ {
 			return this.$.$mol_state_arg.make_link({ snapshot : this.snapshot_current() })
 		}
 		
+		@ $mol_mem
+		speed( next = 0 ) {
+			if( next ) this.$.$mol_dom_context.location.href = this.$.$mol_state_arg.make_link({ snapshot : this.snapshot_current() })
+			return next
+		}
+
 		snapshot() {
 			return this.$.$mol_state_arg.value( 'snapshot' ) ?? super.snapshot()
 		}
