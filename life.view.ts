1bd53b26d2fcbdbae189e31e55bcbb08dd2f4151
jin <jin@hyoo.ru>
2022-09-25 17:02:31 +0300

    + reset

--- life.view.ts
+++ life.view.ts
@@ -9,9 +9,9 @@ namespace $.$$ {
 		store_link() {
 			return this.$.$mol_state_arg.make_link({ snapshot : this.snapshot_current() })
 		}
-
+		
 		snapshot() {
-			return this.$.$mol_state_arg.value( 'snapshot' ) || super.snapshot()
+			return this.$.$mol_state_arg.value( 'snapshot' ) ?? super.snapshot()
 		}
 
 	}
