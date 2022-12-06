477a86ced363fb3f2bafec2c9fcec958743eb9e1
nin-jin <nin-jin@users.noreply.github.com>
2022-12-06 07:16:08 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -5730,6 +5730,8 @@ var $;
                 let pos = this.pointer_center();
                 if (!action_type)
                     return;
+                if (!start_pos)
+                    return;
                 if (action_type === 'draw') {
                     const distance = new $mol_vector(start_pos, pos).distance();
                     if (distance >= 4) {
@@ -5737,8 +5739,6 @@ var $;
                     }
                     return;
                 }
-                if (!start_pos)
-                    return;
                 if (action_type === 'pan') {
                     this.dom_node().setPointerCapture(event.pointerId);
                     this.pan(new $mol_vector_2d(start_pan[0] + pos[0] - start_pos[0], start_pan[1] + pos[1] - start_pos[1]));
@@ -5835,7 +5835,7 @@ var $;
                 const action_type = this.event_eat(event);
                 if (action_type === 'zoom') {
                     const zoom_prev = this.zoom() || 0.001;
-                    const zoom_next = zoom_prev * (1 - .001 * event.deltaY);
+                    const zoom_next = zoom_prev * (1 - .001 * Math.min(event.deltaY, 100));
                     const mult = zoom_next / zoom_prev;
                     this.zoom(zoom_next);
                     const pan_prev = this.pan();
