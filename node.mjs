f3bfdbbbd8f396fb6ac6abee16faf2af4ecf5ade
nin-jin <nin-jin@users.noreply.github.com>
2023-02-27 07:16:56 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.mjs
+++ node.mjs
@@ -3330,7 +3330,6 @@ var $;
                 justifyContent: 'space-between',
                 flex: 'none',
                 margin: 0,
-                overflow: 'hidden',
                 background: {
                     color: $mol_theme.card,
                 },
--- node.test.js
+++ node.test.js
@@ -3322,7 +3322,6 @@ var $;
                 justifyContent: 'space-between',
                 flex: 'none',
                 margin: 0,
-                overflow: 'hidden',
                 background: {
                     color: $mol_theme.card,
                 },
