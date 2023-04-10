7373d5eae19e61e02ee92058a61c41ea4fa3597d
nin-jin <nin-jin@users.noreply.github.com>
2023-04-10 07:14:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1164,7 +1164,7 @@ declare namespace $ {
     class $mol_fetch_response extends $mol_object2 {
         readonly native: Response;
         constructor(native: Response);
-        status(): "success" | "unknown" | "inform" | "redirect" | "wrong" | "failed";
+        status(): "unknown" | "success" | "inform" | "redirect" | "wrong" | "failed";
         code(): number;
         message(): string;
         headers(): Headers;
@@ -1194,8 +1194,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_huggingface_run(this: $, space: string, method: string | number, ...data: readonly any[]): readonly string[];
-    function $mol_huggingface_async(space: string, method: number, ...data: readonly any[]): Promise<[string]> & {
+    function $mol_huggingface_run(this: $, space: string, method: string | number, ...data: readonly any[]): any[] | readonly string[];
+    function $mol_huggingface_async(space: string, method: number, ...data: readonly any[]): Promise<any[]> & {
         destructor: () => void;
     };
 }
@@ -1213,7 +1213,7 @@ declare namespace $ {
         static lang(next?: string): string;
         static source(lang: string): any;
         static texts(lang: string, next?: $mol_locale_dict): $mol_locale_dict;
-        static text(key: string): {} | null;
+        static text(key: string): string;
         static warn(key: string): null;
     }
 }
@@ -1657,7 +1657,7 @@ declare namespace $ {
 declare namespace $ {
     class $mol_lights_toggle extends $mol_check_icon {
         Icon(): $mol_icon_brightness_6;
-        hint(): {} | null;
+        hint(): string;
         checked(val?: any): boolean;
         Lights_icon(): $mol_icon_brightness_6;
         lights(val?: any): boolean;
@@ -1723,7 +1723,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_link_source extends $mol_link {
-        hint(): {} | null;
+        hint(): string;
         sub(): readonly any[];
         Icon(): $mol_icon_github_circle;
     }
@@ -2158,7 +2158,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $hyoo_life extends $mol_page {
-        title(): {} | null;
+        title(): string;
         plugins(): readonly any[];
         head(): readonly any[];
         tools(): readonly any[];
@@ -2176,7 +2176,7 @@ declare namespace $ {
         Reset_icon(): $mol_icon_eraser;
         Reset(): $$.$mol_link;
         store_link(val?: any): string;
-        store_link_hint(): {} | null;
+        store_link_hint(): string;
         Stored(): $mol_icon_stored;
         Store_link(): $$.$mol_link_lazy;
         snapshot(): string;
@@ -2196,7 +2196,7 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $hyoo_life extends $.$hyoo_life {
-        title(): any;
+        title(): string;
         store_link(): string;
         speed(next?: number): number;
         snapshot(): string;
